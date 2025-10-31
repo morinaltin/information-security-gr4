5e2076c516a3fcd2c746e585b376b04149d5eb37
morinaltin <altin.morina6@student.uni-pr.edu>
2025-10-31 12:01:54 +0100

    client flow update

--- auth_elgamal/auth_elgamal/Client/AuthenticationClient.cs
+++ auth_elgamal/auth_elgamal/Client/AuthenticationClient.cs
@@ -1,3 +1,4 @@
+using auth_elgamal;
 using auth_elgamal.Models;
 using auth_elgamal.Services;
 
@@ -9,11 +10,12 @@ public class AuthenticationClient
 
     private ElGamalKeyPair? _keyPair;
     private string? _username;
-    private string? _sessionToken; 
+    private string? _sessionToken;
 
     public string? Username => _username;
     public bool HasKeys => _keyPair is not null;
     public ElGamalPublicKey? PublicKey => _keyPair?.PublicKey;
+    public bool IsAuthenticated => !string.IsNullOrEmpty(_sessionToken);
 
     public AuthenticationClient(AuthenticationService authService)
     {
@@ -36,6 +38,30 @@ public class AuthenticationClient
         }
         return resp;
     }
-    
+
+    public AuthResponse Login(string username)
+    {
+        if (_keyPair is null)
+            return new AuthResponse(false, "No keys available on client");
+
+        _username = username;
+
+        var challenge = _authService.GenerateChallenge(username);
+        if (challenge is null)
+            return new AuthResponse(false, "Failed to get challenge");
+
+        var signature = ElGamalSignatureOps.Sign(challenge.Message, _keyPair);
+
+        var authReq = new AuthRequest(username, challenge.ChallengeId, signature);
+        var resp = _authService.Authenticate(authReq);
+
+        if (resp.Success)
+        {
+            _sessionToken = resp.SessionToken;
+        }
+        return resp;
+    }
+
     public ElGamalKeyPair? GetKeyPair() => _keyPair;
+    public string? GetSessionToken() => _sessionToken;
 }
